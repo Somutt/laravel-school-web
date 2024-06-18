6fc5c8a191317d4533ecd3d50915ea7aa2859fa7
Samuel Moura <samuelmalves1@gmail.com>
2024-06-18 02:18:09 -0300

    auth layout dropdown

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -7,6 +7,8 @@ import { User } from '@/types';
 
 export default function Authenticated({ user, header, children }: PropsWithChildren<{ user: User, header?: ReactNode }>) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
+    const active = route().current('dashboard');
+    console.log(active);
 
     return (
         <div className="flex flex-col min-h-screen bg-gray-100">
@@ -20,10 +22,47 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                                 </Link>
                             </div>
 
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex sm:items-center">
+                                <NavLink href={route('dashboard')} active={active}>
                                     Panel
                                 </NavLink>
+
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className='inline-flex items-center px-3 py-2 border-b-2 border-transparent text-sm leading-4 font-medium text-gray-500 bg-white 
+                                                        hover:text-gray-700 hover:border-gray-300
+                                                        focus:outline-none transition ease-in-out duration-150' 
+                                            >
+                                                <span className='capitalize'>{route().current('dashboard') ? 'management' : route().current()?.slice(0, -6)}</span>
+                                                
+                                                <svg
+                                                    className="ms-2 -me-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <Dropdown.Link href={route('professors.index')} method="get" as="button">
+                                            Professors
+                                        </Dropdown.Link>
+                                        <Dropdown.Link href={route('students.index')} method="get" as="button">
+                                            Students
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
                             </div>
                         </div>
 
