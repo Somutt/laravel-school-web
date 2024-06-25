6cd0d8a798aec79fffebf5685977467061908f55
Samuel Moura <samuelmalves1@gmail.com>
2024-06-25 09:51:51 -0300

    add class form refinement

--- resources/js/Components/AddClass.tsx
+++ resources/js/Components/AddClass.tsx
@@ -18,21 +18,21 @@ export default function AddClass({ professors, rooms }: ClassProps) {
 
     return (
         <form onSubmit={submitClass}>
-            <div className="flex flex-col justify-center items-center w-full">
-                <div className="">
+            <div className="flex justify-center items-center w-full mt-4">
+                <div className="flex items-center">
 
-                    <InputLabel htmlFor="professor" value="Select Professor" className="mt-4"/>
-                    <select id="professor" name="professor" className="w-72" value={data.professor} onChange={ (e) => setData('professor', e.target.value)}>
+                    <InputLabel htmlFor="professor" value="Professor"/>
+                    <select id="professor" name="professor" className="w-72 ml-2" value={data.professor} onChange={ (e) => setData('professor', e.target.value)}>
                         {professors.map( (p) => <option key={p.id} >{p.name}</option> )}
                     </select>
 
-                    <InputLabel htmlFor="room" value="Select Room" className="mt-2"/>
-                    <select id="room" name="room" className="w-72" value={data.room} onChange={ (e) => setData('room', e.target.value) } >
+                    <InputLabel htmlFor="room" value="Room" className="ml-4"/>
+                    <select id="room" name="room" className="w-72 ml-2" value={data.room} onChange={ (e) => setData('room', e.target.value) } >
                         {rooms.map( (r) => <option key={r.id} >{r.name}</option> )}
                     </select>
                 </div>
                 
-                <PrimaryButton className="mt-4">Add Class</PrimaryButton>
+                <PrimaryButton className="ml-4">Add Class</PrimaryButton>
             </div>
         </form>
     );
